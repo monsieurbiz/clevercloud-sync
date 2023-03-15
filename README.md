472dde153d8612ff51a57b0aaa0ef23639b1022e
Maxime Huran <m.huran@monsieurbiz.com>
2023-03-15 09:43:37 +0100

    WIP

--- README.md
+++ README.md
@@ -22,6 +22,8 @@ The application uses some environment variables to run: (those can be in a `$con
 - `CLEVER_TOKEN` and `CLEVER_SECRET` are mandatory as well, both for the synced application and the main application.  
   You *must* create a dedicated member in your Organization so those credentials are safe.
 - `SYNC_APP=app_…` is the code of the sync application itself. The sync script tests this value to be the same as its own `$APP_ID`.
+- `SYNC_BUCKET=project-backups` is the name of the bucket we need to use in the S3 Cellar.
+
 
 Some other environment variables are need for the sync application only (not in the `$config` addon!!):
 - `CC_RUN_COMMAND=./sync.sh` tells Clever Cloud to run the sync script on startup.
@@ -29,7 +31,7 @@ Some other environment variables are need for the sync application only (not in 
 
 Running the sync application will create an environment variable named `SYNC_PLEASE_MY_LOVELY_SCRIPT`, this is perfectly normal.
 
-The backup application also has access to those variables through the linked Cellar:
+The sync application also has access to those variables through the linked Cellar:
 - `CELLAR_ADDON_HOST`
 - `CELLAR_ADDON_KEY_ID`
 - `CELLAR_ADDON_KEY_SECRET`
--- sync.sh
+++ sync.sh
@@ -9,10 +9,10 @@ if [[ "$SYNC_APP" != "$APP_ID" ]]; then
 fi
 
 # Sure?
-if [[ "$SYNC_PLEASE_MY_LOVELY_SCRIPT" != "true" ]]; then
-    echo "This app is running without the safety flag SYNC_PLEASE_MY_LOVELY_SCRIPT set to true. It won't sync."
-    exit 0
-fi
+# if [[ "$SYNC_PLEASE_MY_LOVELY_SCRIPT" != "true" ]]; then
+#     echo "This app is running without the safety flag SYNC_PLEASE_MY_LOVELY_SCRIPT set to true. It won't sync."
+#     exit 0
+# fi
 
 # Do not sync twice in a row, let's be sure of that!
 clever login --token $CLEVER_TOKEN --secret $CLEVER_SECRET
@@ -21,10 +21,29 @@ clever env --alias _myself set SYNC_PLEASE_MY_LOVELY_SCRIPT false
 
 cd $APP_HOME
 
+date=`date '+%Y%m%d%H%M%S'`
+syncDir=$APP_HOME/sync
+chunkSize=30
+
 echo "Sync is started."
 
 # Setup S3
 envsubst < $APP_HOME/s3cfg.dist > $APP_HOME/s3cfg
 
-# List the cellar
-s3cmd -c $APP_HOME/s3cfg ls s3://$BACKUP_BUCKET | sort -r | head -50
+# Check if we have a db to restore
+s3cmd -c $APP_HOME/s3cfg ls s3://$SYNC_BUCKET | while read -r line;
+do
+    # TODO LOGIC
+done;
+
+# Format to use: SYNC_MYSQL(_[0-9]+)?=user:pass:host:port:database:alias
+env | grep SYNC_MYSQL | while read line; do
+    IFS=":" read -r user pass host port db alias <<< `echo $line | awk -F= {'print $2'}`
+    # TODO LOGIC
+    # Download
+    # s3cmd -c $APP_HOME/s3cfg put --multipart-chunk-size-mb=$chunkSize $syncDir/$pathname.$date.tgz s3://$BACKUP_BUCKET
+    # Import dump
+    # zcat <FILEPATH>.sql.gz | mysql -h$host -P$port -u$user -p$pass $db
+    # Clean
+    # rm -f $syncDir/$alias.$db.$date.sql.gz
+done;
